346939326096dad7c3104cce79f9e8e2fa1bae1d
Liam Simmons <liam.simmons@powerhrg.com>
2024-07-19 14:32:11 -0400

    Add methods

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+
+  def actors_list
+    self.characters.map do |c|
+      actor=c.actor
+      "#{actor.first_name} #{actor.last_name}"
+    end
+  end
+
 end
